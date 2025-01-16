1a8ed22ed561defa88d8bc743d59b873923cb626
onv <111040981+onv2487@users.noreply.github.com>
2025-01-16 13:16:53 +0100

    comments

--- src/index.js
+++ src/index.js
@@ -8,17 +8,20 @@ window.onload = function () {
   var peek = document.getElementById("peek");
   var display = document.getElementById("top_of_stack");
 
+  // lägger till en lyssnare för click och tar bort det översta elementet. Meddelande visas
   pop.addEventListener("click", function () {
     var text = "Tog bort " + stack.pop();
     alert(text);
   });
 
+  // lägger till en lyssnare för click och lägger till ett element. Meddelande visas
   push.addEventListener("click", function () {
     var x = prompt("Vad ska vi lägga på stacken?");
     stack.push(x);
     display.innerHTML = x;
   });
 
+  // lägger till en lyssnare för click
   peek.addEventListener("click", function () {
     display.innerHTML = stack.peek();
   });
